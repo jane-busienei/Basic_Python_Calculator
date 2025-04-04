adabd38e2a96f0f274b40d39d51f2b959cfc2e64
jane-busienei <chepchumbajane2@gmail.com>
2025-04-05 00:36:10 +0300

    Basic Python Calculator

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,7 +1,7 @@
 # Prompt the user to enter two numbers and operator
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-operation = input("Enter operation (+, -, *, /): ")
+operation = input("Enter operation (+, -, *, **, %, /): ")
 
 '''Calculation success when conditions are met
 Error handling incase of errors
@@ -16,6 +16,15 @@ elif operation == '-':
 elif operation == '*':
     result = num1 * num2
     print(f"Result: {num1} * {num2} = {result}")
+elif operation == '**':
+    result = num1 ** num2
+    print(f"Result: {num1} ** {num2} = {result}")
+elif operation == '%':
+    if num2 == 0:
+        print("Error: Error: Number not divisible by zero!")
+    else:
+        result = num1 % num2
+        print(f"Result: {num1} % {num2} = {result}")
 elif operation == '/':
     if num2 != 0:
         result = num1 / num2
